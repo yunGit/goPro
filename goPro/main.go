cbe0b30e4c8d67d4242647570106ec8c9f0621c9
unknown <caoguoyun@cyou-inc.com>
2018-05-02 22:03:04 +0800

    what

--- goPro/main.go
+++ goPro/main.go
@@ -1,6 +1,21 @@
 // goPro project main.go
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	i, j := 42, 2701
+
+	p := &i
+	fmt.Println(*p)
+
+	*p = 21
+	fmt.Println(i)
+	fmt.Println("p = ", p)
+
+	p = &j
+	*p = *p / 37
+	fmt.Println(j)
 }
