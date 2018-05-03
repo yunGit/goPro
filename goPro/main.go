30cfd65612082a54ee01db3b55db48c1cb893079
yun <cao_guoyun@163.com>
2018-05-03 23:01:25 +0800

    method

--- goPro/main.go
+++ goPro/main.go
@@ -10,13 +10,32 @@ type Vertex struct {
 	X, Y float64
 }
 
+// go 没有类，但是可以在结构体类型上定义方法
+// 方法的接受者出现在 func关键字和方法名之间的参数中
 func (v *Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+// 可以对保重的 任意 类型定义任意方法，而不仅仅针对结构体
+// 但是，不能对来自其他包的类型或基础类型定义方法
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
 }
 
 func main() {
 	fmt.Println("Hello world!")
 	// 方法
 	v := &Vertex{3, 4}
+	// 结构体方法的调用
 	fmt.Println(v.Abs())
+
+	f := MyFloat(-math.Sqrt2)
+	fmt.Println(f.Abs())
+
+	// next http://go-tour-zh.appspot.com/methods/2
 }
