4497dc01ae27dc288a8e6e4367f6756c9a677bb5
unknown <caoguoyun@cyou-inc.com>
2018-05-14 22:01:16 +0800

    select

--- goPro/main.go
+++ goPro/main.go
@@ -43,24 +43,39 @@ func fibonacci(n int, c chan int) {
 	close(c)
 }
 
+// select
+func fibonacci_select(c, quit chan int) {
+	x, y := 0, 1
+	for {
+		select {
+		case c <- x:
+			fmt.Println("in fibonacci, x = ", x)
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
+	}
+}
+
 func main() {
 	fmt.Println("Hello world!")
 
-	// http://tour.studygolang.com/concurrency/1
-	go say("world")
-	say("hello")
+	//	// http://tour.studygolang.com/concurrency/1
+	//	go say("world")
+	//	say("hello")
 
-	a := []int{7, 2, 4, -4, 3, 0}
-	c := make(chan int)
-	go sum(a[:len(a)/2], c)
-	go sum(a[len(a)/2:], c)
-	x, y := <-c, <-c // 从 c 中获取
-	fmt.Println(x, y, x+y)
+	//	a := []int{7, 2, 4, -4, 3, 0}
+	//	c := make(chan int)
+	//	go sum(a[:len(a)/2], c)
+	//	go sum(a[len(a)/2:], c)
+	//	x, y := <-c, <-c // 从 c 中获取
+	//	fmt.Println(x, y, x+y)
 
 	// 缓冲 channel
 	// channel 可以是 _带缓冲的_，为 make 提供第二个草书作为缓冲长度来初始化一个缓冲 channel
 	// ch := make(chan int, 100)
-	// 向缓冲channel 发送数据的时候，只用在缓冲区慢的时候才会阻塞，当缓冲区清空的时候接触阻塞
+	// 向缓冲channel 发送数据的时候，只用在缓冲区满的时候才会阻塞，当缓冲区清空的时候接触阻塞
 	//	d := make(chan int, 2)
 	//	d <- 1
 	//	d <- 2
@@ -69,15 +84,24 @@ func main() {
 	//	fmt.Println(<-d)
 	//	//	fmt.Println(<-d)
 
-	// range, 循环会从 e 不断的取数据直到 e 被关闭
-	e := make(chan int, 10)
-	go fibonacci(cap(e), e)
-	for i := range e {
-		fmt.Println(i)
-	}
+	//	// range, 循环会从 e 不断的取数据直到 e 被关闭
+	//	e := make(chan int, 10)
+	//	go fibonacci(cap(e), e)
+	//	for i := range e {
+	//		fmt.Println(i)
+	//	}
 
 	// select
 	// select使得一个 goroutine 在多个通讯操作上等待
 	// select会阻塞，直到条件分支中的某个可以继续执行，这时就会执行那个条件分支，当多个都准备好的时候，会随机选择一个
-
+	f := make(chan int)
+	quit := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			//			fmt.Println("in go func, i = ", i)
+			fmt.Println(<-f)
+		}
+		quit <- 0
+	}()
+	fibonacci_select(f, quit)
 }
