8191636f7d1497de8646a084816eab2cb64794de
unknown <caoguoyun@cyou-inc.com>
2018-05-15 20:44:45 +0800

    web reptile

--- goPro/main.go
+++ goPro/main.go
@@ -10,28 +10,54 @@ type Fetcher interface {
 	Fetch(url string) (body string, urls []string, err error)
 }
 
+type MapHasCrawled map[string]string
+
 // Crawl 使用 fetcher 从某个 URL 开始递归的爬取页面，直到达到最大深度。
-func Crawl(url string, depth int, fetcher Fetcher) {
+func Crawl(url string, depth int, fetcher Fetcher, hasFind *MapHasCrawled, b chan string) {
 	// TODO: 并行的抓取 URL。
 	// TODO: 不重复抓取页面。
 	// 下面并没有实现上面两种情况：
 	if depth <= 0 {
 		return
 	}
+
 	body, urls, err := fetcher.Fetch(url)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("found: %s %q\n", url, body)
+	hasFind[url] = body
+
+	var bb chan string
+	bb = make(chan string)
 	for _, u := range urls {
-		Crawl(u, depth-1, fetcher)
+		if hasFind[u] {
+			continue
+		}
+		go Crawl(u, depth-1, fetcher, hasFind, bb)
 	}
+
+	for n := 0; n < cap(urls); n++ {
+		select {
+		case <-bb:
+			continue
+		}
+	}
+	b <- body
 	return
 }
 
 func main() {
-	Crawl("http://golang.org/", 4, fetcher)
+	m := make(MapHasCrawled)
+	var bb chan string
+	bb = make(chan string)
+	go Crawl("http://golang.org/", 4, fetcher, &m, bb)
+	for {
+		select {
+		case <-bb:
+			fmt.Printf("found: http://golang.org/ %q\n", bb)
+		}
+	}
 }
 
 // fakeFetcher 是返回若干结果的 Fetcher。
