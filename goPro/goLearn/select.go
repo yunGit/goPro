7ca76f92d84a543b573d22a020947f801a98ded5
unknown <caoguoyun@cyou-inc.com>
2018-05-15 13:07:07 +0800

    select2

--- goPro/goLearn/select.go
+++ goPro/goLearn/select.go
@@ -141,6 +141,22 @@ func main() {
 
 	receiveMsg()
 
+	//	// select 默认选择 default
+	//	tick := time.Tick(100 * time.Millisecond)
+	//	boom := time.After(500 * time.Millisecond)
+	//	for {
+	//		select {
+	//		case <-tick:
+	//			fmt.Println("tick.")
+	//		case <-boom:
+	//			fmt.Println("BOOM!")
+	//			return
+	//		default:
+	//			fmt.Println("	.")
+	//			time.Sleep(50 * time.Millisecond)
+	//		}
+	//	}
+
 	for {
 
 	}
--- goPro/main.go
+++ goPro/main.go
@@ -5,36 +5,6 @@ import (
 	"fmt"
 )
 
-// select
-func fibonacci_select(c, quit chan int) {
-	x, y := 0, 1
-	for {
-		select {
-		case c <- x:
-			fmt.Println("in fibonacci, x = ", x)
-			x, y = y, x+y
-		case <-quit:
-			fmt.Println("quit")
-			return
-		}
-	}
-}
-
 func main() {
 	fmt.Println("Hello world!")
-
-	f := make(chan int)
-	quit := make(chan int)
-	go func() {
-		for i := 0; i < 10; i++ {
-			//			fmt.Println("in go func, i = ", i)
-			fmt.Println(<-f)
-		}
-		quit <- 0
-	}()
-	fibonacci_select(f, quit)
-
-	for {
-
-	}
 }
